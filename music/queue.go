cf3c489618057a5c5c9e702d28eba3c3ca52be87
Zead0n <141008297+Zead0n@users.noreply.github.com>
2025-09-22 04:18:36 -0500

    Create QueueManager

--- music/queue.go
+++ music/queue.go
@@ -8,10 +8,26 @@ const (
 	LoopQueue
 )
 
-type (
-	Queue struct {
-		list    []Track
-		current *Track
-		mode    LoopMode
+type QueueManager struct {
+	queues map[string]*Queue
+}
+
+func (qm *QueueManager) GetGuildQueue(guildId string) *Queue {
+	queue, ok := qm.queues[guildId]
+	if !ok {
+		queue = &Queue{
+			list:    make([]Track, 0),
+			current: nil,
+			mode:    LoopOff,
+		}
+		qm.queues[guildId] = queue
 	}
-)
+
+	return queue
+}
+
+type Queue struct {
+	list    []Track
+	current *Track
+	mode    LoopMode
+}
