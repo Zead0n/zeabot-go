64e64bd753d11e718b1b9c225dbae6c1ac2a7201
Zead0n <141008297+Zead0n@users.noreply.github.com>
2025-09-29 21:56:42 -0500

    Fix typo

--- internal/discord/utils.go
+++ internal/discord/utils.go
@@ -3,5 +3,5 @@ package discord
 import "github.com/bwmarrin/discordgo"
 
 func (b *Bot) JoinVoiceChannel(guildId, channelId string) (*discordgo.VoiceConnection, error) {
-	return b.session.ChannelVoiceJoin(guildId, channelId, true, false)
+	return b.Session.ChannelVoiceJoin(guildId, channelId, true, false)
 }
