736785df3fcfe9d26652af92c351c0d88950de20
Zead0n <141008297+Zead0n@users.noreply.github.com>
2025-09-30 01:55:30 -0500

    Delete discord package

--- internal/discord/utils.go
+++
@@ -1,7 +0,0 @@
-package discord
-
-import "github.com/bwmarrin/discordgo"
-
-func (b *Bot) JoinVoiceChannel(guildId, channelId string) (*discordgo.VoiceConnection, error) {
-	return b.Session.ChannelVoiceJoin(guildId, channelId, true, false)
-}
